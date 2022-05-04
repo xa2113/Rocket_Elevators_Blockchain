355a89aa36e8e3f1742b7eb75897fc657ce2532e
Felicia H <feli95angeline@gmail.com>
2022-05-04 16:29:40 -0400

    smart contract with added IPFS address

--- src/web3Test.js
+++ src/web3Test.js
@@ -5,9 +5,9 @@ const Web3 = require("web3");
 // import { AbiItem } from "web3-utils";
 const ERC721ABI = require("../build/contracts/RocketNFT.json");
 
-const fs = require("fs");
 // const { default: Web3 } = require("web3");
-const contract = JSON.parse(
+const fs = require("fs");
+const rocketNFT = JSON.parse(
     fs.readFileSync("../build/contracts/RocketNFT.json", "utf8")
 );
 
@@ -15,30 +15,44 @@ const contract = JSON.parse(
 // const abi_string = JSON.stringify(contract.abi);
 
 const url = "http://localhost:7545";
-const contractAddress = "0x9dfAB35B859398f045ee7BE69A42392A4dBb4c7f";
+const contractAddress = "0xAb41ae698C7F0330a68F740D8C47Ce2aCdC50C8B";
 const web3 = new Web3(url);
-const r = new web3.eth.Contract(contract.abi, contractAddress);
+const r = new web3.eth.Contract(rocketNFT.abi, contractAddress);
+
+r.methods.tokenURI(9).call((err, res) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("TOKEN URI: " + res);
+    }
+});
+
+// {
+// “Name”: String
+// “Description”: String
+// “Image”: Link to the source of the image
+// // }
+// let string = r.methods.mintForAccount(
+//     "0x4B507F8f6a7b1650E926483f2b0dcb6FaEc9fcB1",
+//     33
+// );
+// console.log(string);
+// let output = r.methods.balanceOf("0xFB4736eADE1a08c42E65aa187dd32C36E160AAcc");
+// console.log(output);
+
+// Image URI: ipfs://bafkreig5s4oq574drfafphiyazroxxngmid3m6vensuvk3y2qvoz5ubhmi
+
 // var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:7545"));
 
 // var Contract = require("web3-eth-contract");
 // Contract.setProvider(url);
 // var contract2 = new Contract(contract);
-// contract2.methods.balanceOf("0xFB4736eADE1a08c42E65aa187dd32C36E160AAcc");
+// r.methods.balanceOf("0xFB4736eADE1a08c42E65aa187dd32C36E160AAcc");
 
 // console.log(web3);
 // web3.eth.getAccounts().then((accounts) => console.log(accounts));
 // let contractInstance = new web3.eth.Contract(abi_string);
 // console.log(contractInstance);
-
-// r.methods
-//     .getRecor("0x4B507F8f6a7b1650E926483f2b0dcb6FaEc9fcB1")
-//     .call((err, res) => {
-//         if (err) {
-//             console.log(err);
-//         } else {
-//             console.log(res);
-//         }
-//     });
 
 // r.methods
 //     .checkAccount("0xFB4736eADE1a08c42E65aa187dd32C36E160AAcc", 100)
@@ -51,12 +65,12 @@ const r = new web3.eth.Contract(contract.abi, contractAddress);
 //     });
 
 // r.methods
-//     .getRecor("0xFB4736eADE1a08c42E65aa187dd32C36E160AAcc")
+//     .mintForAccount("0xFB4736eADE1a08c42E65aa187dd32C36E160AAcc", 9)
 //     .call((err, res) => {
 //         if (err) {
 //             console.log(err);
 //         } else {
-//             console.log(res);
+//             console.log("MINT FOR ACCOUNT: " + res);
 //         }
 //     });
 
