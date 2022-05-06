b3513ce2268c09cead031a27ee6c3ca241364b57
Felicia H <feli95angeline@gmail.com>
2022-05-06 14:55:47 -0400

    truffle-config fixed - working copy

--- truffle-config.js
+++ truffle-config.js
@@ -8,16 +8,29 @@ module.exports = {
             port: 7545,
             network_id: "*",
         },
-    },
-    matic: {
-        provider: () =>
-            new HDWalletProvider(
-                "lecture quarter sugar pill slot clap brick tent ice forest split deposit",
-                "https://speedy-nodes-nyc.moralis.io/6a20f4bfebb920c2ab0fb82b/polygon/mumbai"
-            ),
-        network_id: 80001,
-        confirmations: 2,
-        timeoutBlocks: 200,
-        skipDryRun: true,
+        matic: {
+            provider: () =>
+                new HDWalletProvider(
+                    "lecture quarter sugar pill slot clap brick tent ice forest split deposit",
+                    "https://speedy-nodes-nyc.moralis.io/6a20f4bfebb920c2ab0fb82b/polygon/mumbai"
+                ),
+            network_id: 80001,
+            confirmations: 2,
+            timeoutBlocks: 200,
+            skipDryRun: true,
+        },
+
+        mumbai: {
+            provider: () =>
+                new HDWalletProvider(
+                    "lecture quarter sugar pill slot clap brick tent ice forest split deposit",
+                    `https://speedy-nodes-nyc.moralis.io/717b1363253ad503ceb93b79/polygon/mumbai`
+                ),
+            network_id: 80001, // Ropsten's id
+            gas: 5500000, // Ropsten has a lower block limit than mainnet
+            confirmations: 2, // # of confs to wait between deployments. (default: 0)
+            timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
+            skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
+        },
     },
 };
